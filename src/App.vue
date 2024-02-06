3614a0ce7c8c66ed4a8901d84f7f77fa92cb801e
Thiago Moura <thiago.moura@atlastechnol.com>
2024-02-06 12:36:56 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<script setup>
+import CounterInc from "./components/CounterInc.vue";
+</script>
+
+<template>
+  <div id="app">
+    <header>
+      <img
+        alt="Vue logo"
+        class="logo"
+        src="./assets/logo.svg"
+        width="125"
+        height="125"
+      />
+
+      <div class="wrapper">
+        <div>
+          <h1>ATR Vue + Jest</h1>
+        </div>
+        <CounterInc />
+      </div>
+    </header>
+  </div>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    flex-direction: column;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/components/CounterInc.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div>Contador: {{ counter }}</div>
+    <button id="addButton" @click="counter++">Adicionar</button>
+    <button id="resetButton" @click="counter = 0">Reiniciar</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "counter-inc",
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+};
+</script>
